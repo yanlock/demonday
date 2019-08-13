fd9e7aa820dc4d3d4d9beedd5b8fafa24c2f7ff9
Yan Fernando Boskoski <yanborkoski21@gmail.com>
2019-08-13 15:48:25 -0300

    criar o cadastro

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Cadastro(models.Model):
+    nome_completo = models.CharField(max_length=100, default='')
+    nome_usuario = models.CharField(max_length=20, default='')
+    email = models.EmailField(max_length=255, default='')
+    celular = models.CharField(max_lentgh=14, default='')
+    senha = models.CharField(max_length=10, default='')
+    foto = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.nome_completo
