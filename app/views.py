11f193bb82c89079f73f49074f31ca12c3742583
Yan Fernando Boskoski <yanborkoski21@gmail.com>
2019-08-12 22:54:12 -0300

    criar função para as pages logi, cadastro. sobre e receitas

--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def mostrar_home(request):
     return render(request,'home.html')
+
+def mostrar_cadastro(request):
+    return render(request,'cadastro.html')
+
+def mostrar_receitas(request):
+    return render(request,'receitas.html')
+
+def mostrar_login(request):
+    return render(request,'login.html')
+
+def mostrar_sobre(request):
+    return render(request,'sobre.html')
